1d15d47eeb6ce0b9fdaf8fa01b66129bb5f0bb33
magicalball <1355061380@qq.com>
2024-12-25 18:19:06 +0800

    添加 RSS

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+
+export async function GET(context) {
+  return rss({
+    title: 'Magicalball | Blog',
+    description: 'Sharing some articles in the Magical House',
+    // site: 'http://localhost:4321/',
+    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    customData: `<language>cn</language>`,
+  })
+}
