77ba8043bc5e96da8c7a0de41eba75132aeccaf1
magicalball <1355061380@qq.com>
2024-12-26 11:51:16 +0800

    添加Preact，新增白天黑天主题切换

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
+import preact from '@astrojs/preact'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  integrations: [preact()],
+})
