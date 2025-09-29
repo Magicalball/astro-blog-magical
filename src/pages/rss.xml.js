c9406a02bae134d9f711f90d2e5f135205c4d218
Magicalball <1355061380@qq.com>
2025-09-29 13:17:03 +0800

    优化适配vercel

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export async function GET(context) {
   return rss({
     title: 'Magicalball | Blog',
     description: 'Sharing some articles in the Magical House',
-    site: 'http://localhost:4321/',
+    site: context.site || 'https://magicalball.top',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>cn</language>`,
   })
